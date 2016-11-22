1fd8ed9b0412264ef17f72eea9db93301fbdaab9
Sanchian-iOS <sanchain@163.com>
2016-11-22 22:05:54 +0800

    工程提交FirstTime

---
+++ UI组件_封装ActionSheet/UI组件_封装ActionSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI组件_封装ActionSheet
+//
+//  Created by ZengSanchain on 2016/11/22.
+//  Copyright © 2016年 ziFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI组件_封装ActionSheet/UI组件_封装ActionSheet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UI组件_封装ActionSheet
+//
+//  Created by ZengSanchain on 2016/11/22.
+//  Copyright © 2016年 ziFei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
